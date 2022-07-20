5cea2b0a7ca309b281962b9116f8b433934d2b66
Amadeo Pellicce <pellicceama@gmail.com>
2022-07-20 12:51:19 +0200

    improving twa ingestor

--- src/tasker/runners/trustwallet.ts
+++ src/tasker/runners/trustwallet.ts
@@ -1,4 +1,4 @@
-import { TokenList } from '@uniswap/token-lists';
+import { TokenInfo, TokenList } from '@uniswap/token-lists';
 import { cloneOrPullRepoAndUpdateSubmodules } from '@map3xyz/assets-helper';
 import { DEFAULT_TEMP_DIR, TRUSTWALLET_REPO } from './../../utils/config';
 import path from 'path';
@@ -20,14 +20,49 @@ export async function getTokenlistFromTrustWallet(network: string): Promise<Toke
 
         // TODO: handle case where the network that is passed does not have a tokenlist format
         // TODO: handle case where we need to map the network name from map3 to the trustwallet network name (or network dir name).. perhaps use the source config field?
-        const tokenlistFile = path.join(getTwaInstanceDirectory(), 'blockchains', network, 'tokenlist-extended.json.json');
+        const tokenlistFile = path.join(getTwaInstanceDirectory(), 'blockchains', network, 'tokenlist.json');
 
-        if(fs.existsSync(tokenlistFile)) {
-            return JSON.parse(fs.readFileSync(tokenlistFile, 'utf8'));
+        if(!fs.existsSync(tokenlistFile)) {
+            throw `Tokenlist file not found for network ${network} on trustwallet`;
         }
 
-        throw `Tokenlist file not found for network ${network} on trustwallet`;
+        let tokenlist: TokenList = JSON.parse(fs.readFileSync(tokenlistFile, 'utf8'));
+
+        // @ts-ignore
+        tokenlist.tokens = tokenlist.tokens.map(t => {
+            const tokenDir = path.join(getTwaInstanceDirectory(), 'blockchains', network, 'assets', t.address);
+
+            if(!fs.existsSync(tokenDir)) {
+                return t;
+            }
+            // expand tokenlist fields with extra available data
+
+            return extractExtraTwaTokenFields(t, path.join(tokenDir, 'info.json'));
+        })
+
+        return tokenlist;
     } catch (err) {
         throw err;
     }
+}
+
+// TODO; perhaps don't call it within getTokenlist, but perhaps have it return a full enhanced map3 asset object instead of TokenInfo and call it in a post processing one
+function extractExtraTwaTokenFields(t: TokenInfo, infoFilePath: string): TokenInfo {
+    if(!fs.existsSync(infoFilePath)) {
+        throw `Cannot extractExtraTwaTokenFields as can't find info file ${infoFilePath}`;
+    }
+
+    const info = JSON.parse(fs.readFileSync(infoFilePath, 'utf-8'));
+
+    if(info.tags && (!t.tags && t.tags.length < info.tags.length)) {
+        // @ts-ignore
+        t.tags = info.tags;
+    }
+
+    if(info.logoURI && t.logoURI) {
+        // @ts-ignore
+        t.logoURI = info.logoURI;
+    }
+
+    return t;
 }
